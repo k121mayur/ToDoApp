6803a91d93d8027f72ce327a055a6ec1fb7834cf
Mayur Kharade <kharade.mayur@gmail.com>
2024-10-27 10:01:29 +0530

    React model pop up added

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,72 +1,74 @@
+import { useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
-import { useState } from "react"
+const ListItem = ({ index, task = "", tasks, setTasks, arrowUpHandeler, arrowDownHandeler, stateHandeler, deleteHandler }) => {
+  const [done, setDone] = useState("d-none");
+  const [showModal, setShowModal] = useState(false);
 
-// eslint-disable-next-line react/prop-types
-const ListItem = ({ index, task="", tasks=[], setTasks }) => {
-  const [done, setDone] = useState("d-none")
-  
-  // const [bg, setbg] = useState("bg-light")
-  // const [textColor, setTextColor] = useState("text-primary")
+  const toggleModal = () => setShowModal(!showModal);
 
   const arrowUpHandeler = (i) => {
-    if (i === 0) {
-      return
-    }
+    if (i === 0) return;
     const newTasks = [...tasks];
     [newTasks[i - 1], newTasks[i]] = [newTasks[i], newTasks[i - 1]];
     setTasks(newTasks);
-  }
+  };
 
   const arrowDownHandeler = (i) => {
-    if (i === tasks.length - 1) {
-      return
-    }
+    if (i === tasks.length - 1) return;
     const newTasks = [...tasks];
     [newTasks[i + 1], newTasks[i]] = [newTasks[i], newTasks[i + 1]];
     setTasks(newTasks);
-    
-  }
+  };
 
   const stateHandeler = () => {
-    setDone(done === "d-none" ? "" : "d-none")
-    
-    let temp_tasks = [...tasks];
-    let updatedTask = {...temp_tasks[index]};
-    updatedTask.markedDone = !updatedTask.markedDone
-    temp_tasks[index] = updatedTask
-    setTasks([...temp_tasks])
-    localStorage.setItem("persistantTasks", JSON.stringify([...temp_tasks]));
-  }
+    setDone(done === "d-none" ? "" : "d-none");
+    const temp_tasks = [...tasks];
+    const updatedTask = { ...temp_tasks[index] };
+    updatedTask.markedDone = !updatedTask.markedDone;
+    temp_tasks[index] = updatedTask;
+    setTasks(temp_tasks);
+    localStorage.setItem("persistantTasks", JSON.stringify(temp_tasks));
+  };
 
   const deleteHandler = () => {
-    const deleteConfirmation = confirm("Are you sure you want to delete this task?");
-    if (deleteConfirmation) {
       const updatedTasks = [...tasks];
       updatedTasks.splice(index, 1);
-      console.log(updatedTasks, "updatedTasks");
-      
-      setTasks(updatedTasks)
-      localStorage.setItem("persistantTasks", JSON.stringify([...updatedTasks]));
-
-    }else{
-      alert("Task not deleted")
-    }
-    
-  }
+      setTasks(updatedTasks);
+      localStorage.setItem("persistantTasks", JSON.stringify(updatedTasks))
+      toggleModal();
+  };
 
   return (
-    <div className={`my-1 ${task.markedDone ? "bg-success" : "bg-light"} col-11 text-left p-1 rounded`}> 
+    <div className={`my-1 ${task.markedDone ? "bg-success" : "bg-light"} col-11 text-left p-1 rounded`}>
       <div className="d-flex justify-content-between p-1">
-        <p className={`${task.markedDone ? "text-white" : "text-primary"} m-0 col-md-8`}>{task.task}</p>
+        <p className={`${task.markedDone ? "text-white" : "text-primary"} m-0 col-md-8`} onClick={toggleModal}>
+          {task.task}
+        </p>
         <div className="col-md-4 col-6 d-flex justify-content-between align-items-center">
-          <i className={`fa-solid fa-arrow-up ${task.markedDone ? "text-white" : "text-primary"}`} onClick={() => {arrowUpHandeler(index)}} ></i>
-          <i className={`fa-solid fa-arrow-down ${task.markedDone ? "text-white" : "text-primary"}`} onClick={() => {arrowDownHandeler(index)}}></i>
-          <input type="checkbox" className="h-100 bg-success" checked={task.markedDone} onChange={() => { stateHandeler()}} />
-          <button className={`btn btn-danger ${task.markedDone ? "" : "d-none"} p-0 px-1`}  style={{height: "20px", fontSize: "10px"}} onClick={deleteHandler}>Delete</button>
+          <i className={`fa-solid fa-arrow-up ${task.markedDone ? "text-white" : "text-primary"}`} onClick={() => arrowUpHandeler(index)}></i>
+          <i className={`fa-solid fa-arrow-down ${task.markedDone ? "text-white" : "text-primary"}`} onClick={() => arrowDownHandeler(index)}></i>
+          <input type="checkbox" className="h-100 bg-success" checked={task.markedDone} onChange={stateHandeler} />
+          <button className={`btn btn-danger ${task.markedDone ? "" : "d-none"} p-0 px-1`} style={{ height: "20px", fontSize: "10px" }} onClick={toggleModal}>Delete</button>
         </div>
       </div>
-    </div>
-  )
-}
 
-export default ListItem
+      {/* Modal for task details */}
+      <Modal show={showModal} onHide={toggleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Do you really want to delete following task?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{task.task}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={toggleModal}>Close</Button>
+          <Button variant="danger" onClick={deleteHandler}>Delete</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default ListItem;
